e98c18f3f63e283141b83eef7033fb3e7d1997de
dubh3401 <hubert@sbquantum.com>
2023-04-19 12:00:57 -0400

    rm build stuff

--- blank_project/CMake/toolchain.cmake
+++ blank_project/CMake/toolchain.cmake
@@ -7,7 +7,25 @@ message("the path is : " ${TOOLCHAIN_PATH})
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc.exe)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/arm-none-eabi-g++.exe)
-set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+
+# set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+message("Compiler Version:" ${CMAKE_CXX_COMPILER_VERSION})
+
+add_compile_options(
+
+    # --specs=nosys.specs
+    -mcpu=cortex-m4
+    -mfpu=fpv4-sp-d16
+    -mfloat-abi=hard
+)
+
+add_link_options(
+
+    # --specs=nosys.specs
+    -mcpu=cortex-m4
+    -mfpu=fpv4-sp-d16
+    -mfloat-abi=hard
+)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
