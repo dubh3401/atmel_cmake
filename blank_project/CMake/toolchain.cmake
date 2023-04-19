c5315174b9eacd5c06a01d26aab65aac79e1852f
dubh3401 <hubert@sbquantum.com>
2023-04-19 17:37:23 -0400

    added .elf extension and stopped initial compiler tests

--- blank_project/CMake/toolchain.cmake
+++ blank_project/CMake/toolchain.cmake
@@ -5,16 +5,21 @@ set(CMAKE_MAKE_PROGRAM "C:/MinGW/bin")
 set(TOOLCHAIN_PATH "C:/SysGCC/arm-eabi")
 message("the path is : " ${CMAKE_CURRENT_SOURCE_DIR})
 
+# definition of compilers
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc.exe)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/arm-none-eabi-g++.exe)
 
+# stop compilers check, otherwise linker fails in cmake configuration steps
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+
+# define linker script to use
 set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/src/Device_Startup/same51n20a_flash.ld")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${LINKER_SCRIPT}")
+
 message("Compiler Version:" ${CMAKE_CXX_COMPILER_VERSION})
 
 add_compile_options(
-
-    # --specs=nosys.specs
     -x
     c
     -mthumb
@@ -26,6 +31,8 @@ add_compile_options(
     -mcpu=cortex-m4
     -mfpu=fpv4-sp-d16
     -mfloat-abi=hard
+    -c
+    -MD -MP -MF "$(@:%.o=%.d)" -MT "$(@:%.o=%.d)" -MT "$(@:%.o=%.o)"
 )
 
 add_link_options(
@@ -34,9 +41,5 @@ add_link_options(
     -mcpu=cortex-m4
     -mfpu=fpv4-sp-d16
     -mfloat-abi=hard
+    -Xlinker --print-memory-usage
 )
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
